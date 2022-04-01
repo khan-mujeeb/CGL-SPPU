71d727038d4eb3c38df3c499e06f85cc0f0934af
Mujeeb Khan <89351750+khan-mujeeb@users.noreply.github.com>
2022-04-01 23:34:11 +0530

    Add files via upload

---
+++ square/sq.cpp
@@ -0,0 +1,35 @@
+
+// Header file
+	#include<iostream>
+	#include<GL/glut.h>
+	
+	void Draw(){
+		glClear(GL_COLOR_BUFFER_BIT);
+		
+		glPointSize(5);
+		glBegin(GL_LINE_LOOP);
+			
+			glVertex2f(0.5,0.5);
+			glVertex2f(0.5,-0.5);
+			glVertex2f(-0.5,-0.5);
+			glVertex2f(-0.5,0.5);
+		glEnd();
+		
+		glFlush();
+	}
+	
+	int main(int c, char*V[]) {
+		glutInit(&c,V);
+		glutInitWindowSize(700,600);	// w h
+		glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+		glutCreateWindow("Basic opengl program");
+		
+		glClearColor(0,0,0,1);	//set background color
+		glColor3f(1,1,1);
+		glutDisplayFunc(Draw);	// display calll back function 
+		glutMainLoop();
+		
+		return 0;
+	} 
+
+
